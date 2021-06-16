48df74dec343c6ab41faafa64d9ab372c6cfe157
Raja Kunal Pandit <76692648+RajaKunalPandit1@users.noreply.github.com>
2021-06-16 16:57:43 +0530

    Create Finding Square Roots.cpp
    In olden days finding square roots seemed to be difficult but nowadays it can be easily done using in-built functions available across many languages .
    
    Assume that you happen to hear the above words and you want to give a try in finding the square root of any given integer using in-built functions. So here's your chance.
    
    Input
    The first line of the input contains an integer T, the number of test cases. T lines follow. Each line contains an integer N whose square root needs to be computed.
    
    Output
    For each line of the input, output the square root of the input integer, rounded down to the nearest integer, in a new line.
    
    Constraints
    1<=T<=20
    1<=N<=10000
    Input:
    3
    10
    5
    10000
    
    Output:
    3
    2
    100

---
+++ Finding Square Roots.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	 int t;
+	 cin>>t;
+	 while(t--){
+	    int n;
+	    cin>>n;
+	    int i=0;
+	    while(i*i<=n){
+	        i++;
+	    }
+	    cout<< (i-1) << endl;
+	     
+	     
+	 }
+	 
+	return 0;
+}
